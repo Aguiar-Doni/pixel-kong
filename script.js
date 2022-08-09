e13154a44fd0493a7f3cac2bfd7acc2907d03374
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-08-09 19:22:49 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+
 window.onload = function() {
     let firstColor = document.querySelector(".color");
-    firstColor.classList.add('selected')
+    firstColor.classList.add('selected');
+
+    let colorPalette = document.querySelector('#color-palette')
+    
+    colorPalette.addEventListener('click', function(event){
+    let elementClassName = document.querySelector('.selected');
+    elementClassName.classList.remove('selected')  
+        
+    let select = event.target;
+    select.classList.add('selected')  
+        
+        console.log(event.target)
+    })
 }
+
+
+

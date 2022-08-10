b1851f99c912082be6c7f2a261625c9b2bdeae85
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-08-09 22:42:44 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 
+
 window.onload = function() {
     let firstColor = document.querySelector(".color");
     firstColor.classList.add('selected');
 
     let colorPalette = document.querySelector('#color-palette')
-    
     colorPalette.addEventListener('click', function(event){
+    
     let elementClassName = document.querySelector('.selected');
     elementClassName.classList.remove('selected')  
         
     let select = event.target;
     select.classList.add('selected')  
-        
-        console.log(event.target)
-    })
+    console.log(event.target);
+})
+    let elementClassName = document.querySelectorAll('.pixel');
+    
+    for (index = 0; index < elementClassName.length; index += 1) {
+    elementClassName[index].addEventListener("click", color) 
 }
+    function color(e){
+    let recebe = e.target;
+    let colorPalette2 = document.querySelectorAll('.color')
+    for(let index2 = 0; index2 < colorPalette2.length; index2 += 1) {
+    let variador = colorPalette2[index2].classList.contains('selected'); 
+    if (variador === true) {
+    recebe.className = 'pixel ' + colorPalette2[index2].classList[1];
+}        
+}
+}
+}
+
+
 
 
 
